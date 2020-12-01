7bfd0c65f8c4ff4c71ef5eab837b6f106875b46f
k3v1n <k3v1n0x90@gmail.com>
2020-12-01 07:56:58 +0100

    Solve day 1

--- src/day1/problem.js
+++ src/day1/problem.js
@@ -1,4 +1,60 @@
 const readInput = require('../util/inputReader')
 
-readInput(__dirname)
-  .then(console.log)
+function solve1(lines, target) {
+  let a = 0
+  let b = 0
+
+  for (let i = 0; i < lines.length; ++i) {
+    a = parseInt(lines[i].trim(), 10)
+
+    for (let k = i + 1; k < lines.length; ++k) {
+      b = parseInt(lines[k].trim(), 10)
+
+      if (a + b === target) {
+        return a * b
+      }
+    }
+  }
+
+  return undefined
+}
+
+function solve2(lines, target) {
+  let a = 0
+  let b = 0
+  let c = 0
+
+  for (let i = 0; i < lines.length; ++i) {
+   a = parseInt(lines[i].trim(), 10)
+
+   for (let k = i + 1; k < lines.length; ++k) {
+     b = parseInt(lines[k].trim(), 10)
+
+     for (let j = k + 1; j < lines.length; ++j) {
+       c = parseInt(lines[j].trim(), 10)
+
+       if (a + b + c === target) {
+         return a * b * c
+       }
+     }
+   }
+  }
+
+  return undefined
+}
+
+function solve(part, input) {
+  const target = 2020
+  const lines = input.split('\n')
+
+  if (part === 1) {
+    return solve1(lines, target)
+  }
+
+  return solve2(lines, target)
+}
+
+module.exports = function (part) {
+  return readInput(__dirname, part)
+    .then(solve.bind(this, part))
+}
--- src/util/inputReader.js
+++ src/util/inputReader.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function (directory) {
-  const inputPath = path.join(directory, 'input.txt')
+module.exports = function (directory, part) {
+  const inputPath = path.join(directory, `input${part}.txt`)
 
   return new Promise((resolve, reject) => {
     fs.readFile(inputPath, 'utf-8', (err, data) => {
