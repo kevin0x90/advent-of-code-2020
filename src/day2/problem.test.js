2763763524dfa7cee664f111e1fbfe2573394cb9
k3v1n <k3v1n0x90@gmail.com>
2020-12-02 08:22:03 +0100

    Day 2 adjust test names

--- src/day2/problem.test.js
+++ src/day2/problem.test.js
@@ -4,7 +4,7 @@ const inputReader = require('../util/inputReader')
 jest.mock('../util/inputReader')
 
 describe('day 1', () => {
-  it('part 1 - find product of 2 numbers sum up to 2020', () => {
+  it('part 1 - number of valid passwords with min, max occurence of a character', () => {
     inputReader.mockResolvedValue(`1-3 a: abcde
       1-3 b: cdefg
       2-9 c: ccccccccc`)
@@ -14,7 +14,7 @@ describe('day 1', () => {
     return expect(result).resolves.toEqual(2)
   })
 
-  it('part 2 - find product of 3 numbers sum up to 2020', () => {
+  it('part 2 - number of valid passwords with exclusive or occurence', () => {
     inputReader.mockResolvedValue(`1-3 a: abcde
       1-3 b: cdefg
       2-9 c: ccccccccc`)
